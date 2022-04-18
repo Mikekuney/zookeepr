2e77a676e7f0604347ea9af036ba72db25527fab
Mikekuney <96913841+Mikekuney@users.noreply.github.com>
2022-04-18 18:53:03 -0400

    added new get() and findById()

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const PORT = processs.env.PORT || 3001;
+const res = require('express/lib/response');
+const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require('./data/animals');
 
@@ -43,12 +44,26 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+    res.send(404);
+    }
 });
 
 
